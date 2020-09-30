e53a60bec003cd99477d68a570002082279f6517
Stanislaw <val_zur@mail.ru>
2020-09-30 17:25:01 +0300

    ADEDD : Two Interfaces

---
+++ src/com/content2/Consumable.java
@@ -0,0 +1,5 @@
+package com.content2;
+
+public interface Consumable {
+    public abstract void consume();
+}
---
+++ src/com/content2/Nutritious.java
@@ -0,0 +1,5 @@
+package com.content2;
+
+public interface Nutritious {
+    public abstract Double calculateCalories();
+}
